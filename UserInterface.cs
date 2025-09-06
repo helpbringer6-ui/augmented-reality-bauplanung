97a3bc62928f529a9d9cf8da8f2cb28438ade074
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-09-06 14:00:07 -0500

    feat: initial commit for UserInterface.cs

---
+++ UserInterface.cs
@@ -0,0 +1,12 @@
+// Benutzeroberfläche für Augmented Reality Bauplanung
+
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    public void ShowPlanningOptions()
+    {
+        // Logik zur Darstellung der Planungsoptionen
+        Debug.Log("Planungsoptionen angezeigt.");
+    }
+}
